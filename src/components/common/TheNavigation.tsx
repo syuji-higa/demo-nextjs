a440ca849aa0c6a221a29334427e40872c058c0a
syuji-higa <syuji-higa@gmail.com>
2020-03-14 11:57:08 +0900

    import React -> import * as React

--- src/components/common/TheNavigation.tsx
+++ src/components/common/TheNavigation.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import * as React from 'react'
 import { NextPage } from 'next'
 import Link from 'next/link'
 import styled from 'styled-components'
--- src/components/modules/DefaultButton.tsx
+++ src/components/modules/DefaultButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import * as React from 'react'
 import { NextPage } from 'next'
 import styled from 'styled-components'
 import media from 'styled-media-query'
